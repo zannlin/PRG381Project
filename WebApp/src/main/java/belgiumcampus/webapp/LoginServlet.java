d523e1a793f4e5dc384b89dfffd74d61d6e36b77
TylerG14 <tyler.geuens@gmail.com>
2025-07-09 17:06:43 +0200

    Updated loginservlet for testing

--- WebApp/src/main/java/belgiumcampus/webapp/LoginServlet.java
+++ WebApp/src/main/java/belgiumcampus/webapp/LoginServlet.java
@@ -1,4 +1,27 @@
 package belgiumcampus.webapp;
 
-public class LoginServlet {
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "LoginServlet", value = "/login-servlet")
+public class LoginServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
 }
