7a3e9f494eaa4dbf30fcdd0a3ebd6d313c9bf230
simonscrambled <soerienel@gmail.com>
2025-07-14 13:04:08 +0200

    feat: doPost string values

--- WebApp/src/main/java/belgiumcampus/webapp/RegisterServlet.java
+++ WebApp/src/main/java/belgiumcampus/webapp/RegisterServlet.java
@@ -4,6 +4,7 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.*;
+import org.mindrot.jbcrypt.BCrypt;
 
 @WebServlet(name = "registerServlet", value = "/register-servlet")
 public class RegisterServlet extends HttpServlet {
@@ -19,6 +20,19 @@ public class RegisterServlet extends HttpServlet {
         request.getRequestDispatcher("register.jsp").forward(request, response);
     }
 
-    public void destroy() {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String studentNumber= request.getParameter("txtStudentNumber");
+        String name= request.getParameter("txtName");
+        String surname= request.getParameter("txtSurname");
+        String email= request.getParameter("txtEmail");
+        String phoneNumber= request.getParameter("txtPhoneNumber");
+        //hash password
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        //connection
+
+    }
+
+        public void destroy() {
     }
 }
